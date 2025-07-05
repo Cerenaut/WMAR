3a1c14a6f87d7926b0a8ba9293ad2dc13351aea7
Abdulaziz <119353586+AzizGW@users.noreply.github.com>
2025-07-05 14:53:24 +0300

    Update train.py

--- train.py
+++ train.py
@@ -11,7 +11,7 @@ import torch
 from torch.optim import Adam
 from torch.utils.tensorboard import SummaryWriter
 from tqdm import trange
-
+import ale_py
 import replay
 from ac import ActorCriticOpt, train_ac_from_wm
 from config import Config, EnvConfig, EnvScheduleConfig, RbConfig
